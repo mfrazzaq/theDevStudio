b028bec9727fc5f3189d445f5ebbb92ac2893e26
imfazeelrazzaq <frrough@gmail.com>
2022-09-08 17:00:34 +0500

    In progress

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,24 @@
-const btn = document.getElementById("menu-button")
-const nav = document.getElementById("mobile-menu")
+const btn = document.getElementById("menu-button");
+const nav = document.getElementById("mobile-menu");
+const requestAQuote = document.getElementById("quote");
+const body = document.getElementsByName("body");
+const navbar = document.getElementById("navbar");
 
 function onToggle() {
-    btn.classList.toggle('open')
-    nav.classList.toggle('hidden')
-    document.body.classList.toggle("no-scroll")
+  btn.classList.toggle("open");
+  nav.classList.toggle("hidden");
+  document.body.classList.toggle("no-scroll");
 }
 
-btn.addEventListener('click', onToggle)
+function onRequestingAQuote() {
+  document.getElementById("contact").scrollIntoView();
+}
+
+function onScroll() {
+  console.log("Scrolled");
+  navbar.classList.toggle("fix");
+}
+
+btn.addEventListener("click", onToggle);
+requestAQuote.addEventListener("click", onRequestingAQuote);
+body.addEventListener("scroll", onScroll);
