e097849eebf4da001b27ad8ea165bb28e6e21aa9
imfazeelrazzaq <frrough@gmail.com>
2022-09-08 18:11:51 +0500

    removing an error

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,6 @@
 const btn = document.getElementById("menu-button");
 const nav = document.getElementById("mobile-menu");
 const requestAQuote = document.getElementById("quote");
-const home = document.getElementsByClassName("nav-home");
-const services = document.getElementsByClassName("nav-services");
-const work = document.getElementsByClassName("nav-work");
-const clients = document.getElementsByClassName("nav-clients");
-const contact = document.getElementsByClassName("nav-contact");
 
 function onToggle() {
   btn.classList.toggle("open");
@@ -19,9 +14,3 @@ function onRequestingAQuote() {
 
 btn.addEventListener("click", onToggle);
 requestAQuote.addEventListener("click", onRequestingAQuote);
-services.onclick = onToggle;
-home.addEventListener("click", onToggle);
-services.addEventListener("click", onToggle);
-work.addEventListener("click", onToggle);
-clients.addEventListener("click", onToggle);
-contact.addEventListener("click", onToggle);
